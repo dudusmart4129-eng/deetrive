5b4a670af3d9bde930376eab12494a62536759f5
Deetrive <dudusmart4129@gmail.com>
2026-09-23 21:08:38 +0100

    Deploy updated Deetrive dashboard

--- assets/dashboard-BzK_ovcC.js
+++
@@ -1,1 +0,0 @@
-import{n as e,r as t,t as n}from"./wix-client-BcyrvGQV.js";/* empty css              */t((()=>{n(),e.auth.init().then(()=>{document.querySelector(`#logout-btn`).addEventListener(`click`,async()=>{try{let t=window.location.href,{logoutUrl:n}=await e.auth.logout(t);window.location.href=n}catch(e){console.error(`Logout error:`,e),alert(`Unable to log out right now.`)}})})}))();
