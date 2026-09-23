f64ea68d6482e2a55d235c778fd7ac7d15885176
Deetrive <dudusmart4129@gmail.com>
2026-09-23 01:23:12 +0100

    Fix persistent Wix authentication

--- wix-client.js
+++ wix-client.js
@@ -1,7 +1,20 @@
 import { createClient, OAuthStrategy } from '@wix/sdk'
 
-export const wixClient = createClient({
-  auth: OAuthStrategy({
-    clientId: '50a1463b-3a58-4b5e-8d47-8b886771326e'
-  })
+const authStrategy = OAuthStrategy({
+  clientId: '50a1463b-3a58-4b5e-8d47-8b886771326e'
 })
+
+export const wixClient = createClient({
+  auth: authStrategy
+})
+
+const savedTokens = localStorage.getItem('wixTokens')
+
+if (savedTokens) {
+  try {
+    wixClient.auth.setTokens(JSON.parse(savedTokens))
+  } catch (error) {
+    console.error('Unable to restore Wix session:', error)
+    localStorage.removeItem('wixTokens')
+  }
+}
